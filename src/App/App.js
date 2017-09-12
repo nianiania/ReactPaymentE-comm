4550ea1f4a234ba6dd870181124fe46a7aab68c5
kurnia22 <kurniawati.22.nk@gmail.com>
2017-09-12 18:11:25 +0700

    almost done

--- src/App/App.js
+++ src/App/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Payment from '../payment/payment';
+import ProductPayment from '../product_payment/product_payment';
 
 
 
@@ -16,7 +17,8 @@ class App extends Component {
 
           <div className="container App-main">
             <div className="row">
-               <Payment />
+               <ProductPayment className="col-sm-6" price="12.000.000" title="Macbook Pro" imgUrl="https://store.storeimages.cdn-apple.com/4974/as-images.apple.com/is/image/AppleInc/aos/published/images/m/bp/mbp15touch/gray/mbp15touch-gray-select-201610?wid=452&hei=420&fmt=jpeg&qlt=95&op_sharpen=0&resMode=bicub&op_usm=0.5,0.5,0,0&iccEmbed=0&layer=comp&.v=1496611018929" />
+               <Payment className="col-sm-6"/>
             </div>
           
           </div>
--- src/payment/payment.js
+++ src/payment/payment.js
@@ -4,111 +4,76 @@ import './payment.css';
 class Payment extends Component{
     render(){
         return(
-            <div className="container">
-
-                <div className="card product">
-                    <img className="card-img-top" alt="Product" ></img>
-                    <div className="card-block">
-                        <h4 className="card-title">Macbook</h4>
-                        <p className="card-text">Price: Rp</p>
-                    </div>
-                </div>
-
                 <div className="card payment">
                     <div className="card-block">
                         <form>
-                          <div className="form-group">
-                            <label>
+                          <h4> Pembayaran Via Kartu Visa/Mastercard/JCB</h4>
+                            <label className="deskripsi">
                             Nomor Kartu
-                            <input type="text" name="name" />
+                            <br/>
+                            <input type="text" className="nomorkartu" />
                             </label>
-                          </div>
+                          
                           <div className ="form-inline">
-                                <label for="exampleSelect1">Waktu Kadaluarsa</label>
-                                <div className="btn-group" role="group">
-                                    <button id="btnGroupDrop1" type="button" className="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                      Bulan
-                                    </button>
-                                    <div className="dropdown-menu" aria-labelledby="btnGroupDrop1">
-                                      <a className="dropdown-item" href="#">1</a>
-                                      <a className="dropdown-item" href="#">2</a>
-                                      <a className="dropdown-item" href="#">3</a>
-                                      <a className="dropdown-item" href="#">4</a>
-                                      <a className="dropdown-item" href="#">5</a>
-                                      <a className="dropdown-item" href="#">6</a>
-                                      <a className="dropdown-item" href="#">7</a>
-                                      <a className="dropdown-item" href="#">8</a>
-                                      <a className="dropdown-item" href="#">9</a>
-                                      <a className="dropdown-item" href="#">10</a>
-                                      <a className="dropdown-item" href="#">11</a>
-                                      <a className="dropdown-item" href="#">12</a>
-                                    </div>
-                                </div>
-                                <div className="btn-group" role="group">
-                                 <button id="btnGroupDrop2" type="button" className="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                      Tahun
-                                    </button>
-                                    <div className="dropdown-menu" aria-labelledby="btnGroupDrop1">
-                                      <a className="dropdown-item" href="#">2017</a>
-                                      <a className="dropdown-item" href="#">2018</a>
-                                      <a className="dropdown-item" href="#">2019</a>
-                                      <a className="dropdown-item" href="#">2020</a>
-                                      <a className="dropdown-item" href="#">2021</a>
-                                      <a className="dropdown-item" href="#">2022</a>
-                                      <a className="dropdown-item" href="#">2023</a>
-                                      <a className="dropdown-item" href="#">2024</a>
-                                      <a className="dropdown-item" href="#">2025</a>
-                                      <a className="dropdown-item" href="#">2026</a>
-                                      <a className="dropdown-item" href="#">2027</a>
-                                      <a className="dropdown-item" href="#">2028</a>
-                                      <a className="dropdown-item" href="#">2029</a>
-                                      <a className="dropdown-item" href="#">2030</a>
-                                      <a className="dropdown-item" href="#">2031</a>
-                                      <a className="dropdown-item" href="#">2032</a>
-                                    </div>
-                                </div>
+                                <label className="dua">Waktu Kadaluarsa</label>
                                 
-                                <label>
+                                
+                                <select id="ddlCars">
+                                    <option value="">Bulan</option>
+                                    <option className="dropdown-item">1</option>
+                                    <option className="dropdown-item">2</option>
+                                    <option className="dropdown-item">3</option>
+                                    <option className="dropdown-item">4</option>
+                                    <option className="dropdown-item">5</option>
+                                    <option className="dropdown-item">6</option>
+                                    <option className="dropdown-item">7</option>
+                                    <option className="dropdown-item">8</option>
+                                    <option className="dropdown-item">9</option>
+                                    <option className="dropdown-item">10</option>
+                                    <option className="dropdown-item">11</option>
+                                    <option className="dropdown-item">12</option>
+                                </select>
+                                
+                                <select id="ddlCars">
+                                    <option value="">Tahun</option>
+                                    <option className="dropdown-item">2017</option>
+                                    <option className="dropdown-item">2018</option>
+                                    <option className="dropdown-item">2019</option>
+                                    <option className="dropdown-item">2020</option>
+                                    <option className="dropdown-item">2021</option>
+                                    <option className="dropdown-item">2022</option>
+                                    <option className="dropdown-item">2023</option>
+                                    <option className="dropdown-item">2024</option>
+                                    <option className="dropdown-item">2025</option>
+                                    <option className="dropdown-item">2026</option>
+                                    <option className="dropdown-item">2027</option>
+                                    <option className="dropdown-item">2028</option>
+                                </select>
+
+                                
+                               
+                                <label className="cvv">
                                 CVV
-                                <input type="text" name="name" />
+                                <input type="text" className="CV" />
                                 </label>
                                 
 
                           </div>
+                          <br/>
 
-                          <div className ="form-inline">
-                          <button type="submit" class="btn btn-primary">Cancel</button>
-                          <button type="submit" class="btn btn-primary">Pay</button>
-                          </div>
+                          
+                          <button type="submit" className="btn btn-primary float-left" >Cancel</button>
+                          <button type="submit" className="btn btn-primary float-right">Pay</button>
+                          
 
                     </form>
                     </div>
                 </div>
 
-        </div>
+        
 
 
-         // <form className="form-inline">
-         //      <label className="mr-sm-2" for="inlineFormCustomSelect">Preference</label>
-         //      <select className ="custom-select mb-2 mr-sm-2 mb-sm-0" id="inlineFormCustomSelect">
-         //        <option selected>Choose...</option>
-         //        <option value="1">One</option>
-         //        <option value="2">Two</option>
-         //        <option value="3">Three</option>
-         //      </select>
-
-         //      <label className="custom-control custom-checkbox mb-2 mr-sm-2 mb-sm-0">
-                
-         //        <span className="custom-control-indicator"></span>
-         //        <span className="custom-control-description">Remember my preference</span>
-         //      </label>
-
-         //      <button type="submit" class="btn btn-primary">Submit</button>
-         //      <label>
-         //         Name:
-         //        <input type="text" name="name" />
-         //    </label>
-         // </form>
+         
         );
     }
 }
